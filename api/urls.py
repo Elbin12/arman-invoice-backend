b52e5b37e19aa44216a4a420b61338c3d4cde187
Elbin12 <elbinshiby14@gmail.com>
2025-07-14 19:26:24 +0530

    job create url added

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from .views import ServicesView, ContactsView, GHLUserSearchView ,webhook_handler
+from .views import ServicesView, ContactsView, GHLUserSearchView, CreateJob, webhook_handler
 
 urlpatterns = [
     path("webhook",webhook_handler),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('services/', ServicesView.as_view()),
     path('contacts/', ContactsView.as_view()),
-    path('users/', GHLUserSearchView.as_view())
+    path('users/', GHLUserSearchView.as_view()),
+    path('create/job/', CreateJob.as_view()),
 ]
