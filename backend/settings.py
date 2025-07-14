2724a5b3823f142e616c72e72f88872975eb896e
Elbin12 <elbinshiby14@gmail.com>
2025-07-14 23:28:08 +0530

    allowed host;

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.58.139.11']
 
 
 # Application definition
@@ -55,11 +55,13 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
+    "http://13.58.139.11/"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
+    "http://13.58.139.11/"
 ]
 
 MIDDLEWARE = [
