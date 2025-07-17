975ca12627ee557160f1f12d34306d81badaa9e7
Elbin12 <elbinshiby14@gmail.com>
2025-07-17 22:20:04 +0530

    small fix;

--- ghl_auth/urls.py
+++ ghl_auth/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("connect/", auth_connect, name="oauth_connect"),
     path("tokens/", tokens, name="oauth_tokens"),
     path("callback/", callback, name="oauth_callback"),
+    path('refresh/', TokenRefreshView.as_view()),
 ]
