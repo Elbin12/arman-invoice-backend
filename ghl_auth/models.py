8dcff94dd6ed3d51f41259cfb697a5ef1c7f8fc7
Elbin12 <elbinshiby14@gmail.com>
2025-07-23 03:37:18 +0530

    change in GHLUser model;

--- ghl_auth/models.py
+++ ghl_auth/models.py
@@ -18,11 +18,11 @@ class GHLAuthCredentials(models.Model):
     
 class GHLUser(models.Model):
     user_id = models.CharField(max_length=50, unique=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    name = models.CharField(max_length=200)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(null=True)
+    phone = models.CharField(max_length=20, null=True)
     calendar_id = models.CharField(max_length=50, null=True, blank=True)
     location_id = models.CharField(max_length=50, null=True, blank=True, default="")
     percentage = models.DecimalField(decimal_places=2, max_digits=10, default=20)
